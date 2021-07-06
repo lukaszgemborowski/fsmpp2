eaf9c02f79e6ee32b5314caf1b74b582e0976bcc
Łukasz Gemborowski <lukasz.gemborowski@gmail.com>
2021-07-06 17:27:48 +0200

    unit tests

--- tests/tests_state_hierarchy.cxx
+++ tests/tests_state_hierarchy.cxx
@@ -4,10 +4,16 @@
 namespace
 {
 
+struct InnerEvent : fsmpp2::event {};
+struct InnerEventIgnore : fsmpp2::event {};
+struct OuterEvent : fsmpp2::event {};
+
 struct Context
 {
     bool innerCreated = false;
+    bool innerEventHandled = false;
     bool outerCreated = false;
+    bool outerEventHandled = false;
 };
 
 struct InnerState : fsmpp2::state<Context>
@@ -16,6 +22,15 @@ struct InnerState : fsmpp2::state<Context>
         : ctx_ {ctx}
     {
         ctx_.innerCreated = true;
+    }
+
+    auto handle(InnerEvent const&) const {
+        ctx_.innerEventHandled = true;
+        return handled();
+    }
+
+    auto handle(InnerEventIgnore const&) const {
+        return not_handled();
     }
 
     Context& ctx_;
@@ -29,16 +44,50 @@ struct OuterState : fsmpp2::state<Context, fsmpp2::states<InnerState>>
         ctx_.outerCreated = true;
     }
 
+    auto handle(OuterEvent const&) const {
+        ctx_.outerEventHandled = true;
+        return handled();
+    }
+
+    auto handle(InnerEventIgnore const&) const {
+        ctx_.outerEventHandled = true;
+        return handled();
+    }
+
     Context& ctx_;
 };
 
 }
 
-TEST_CASE("Outer and inner state creation", "[states][state][hierarchy]")
+TEST_CASE("Outer and inner state creation and event handling", "[states][state][hierarchy]")
 {
     Context context;
     fsmpp2::states<OuterState> sm{context};
 
     REQUIRE(context.outerCreated);
     REQUIRE(context.innerCreated);
+    REQUIRE(context.innerEventHandled == false);
+    REQUIRE(context.outerEventHandled == false);
+
+    SECTION("Handle event in outer state") {
+        // InnerState does not have OuterEvent handled, therefore event is passed to parent state
+        sm.handle(OuterEvent{});
+        REQUIRE(context.innerEventHandled == false);
+        REQUIRE(context.outerEventHandled == true);
+    }
+
+    SECTION("Handle event in inner state") {
+        // InnerState have handler for InnerEvent, it consumes the event by call to handle()
+        sm.handle(InnerEvent{});
+        REQUIRE(context.innerEventHandled == true);
+        REQUIRE(context.outerEventHandled == false);
+    }
+
+    SECTION("Inner state explicitly call not_handled()") {
+        // InnerState have InnerEventIgnore handler but it ignores it by calling not_handled(),
+        // event should be passed to its parent state
+        sm.handle(InnerEventIgnore{});
+        REQUIRE(context.innerEventHandled == false);
+        REQUIRE(context.outerEventHandled == true);
+    }
 }
